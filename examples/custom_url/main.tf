b765949d6e4a24fc90d2f3d395d350cbd7630db0
John Favorite <john.favorite@actioniq.com>
2022-09-06 13:20:00 -0400

    feat: Allow custom url with Route53

---
+++ examples/custom_url/main.tf
@@ -0,0 +1,12 @@
+module "retool-self-hosted" {
+  source = "terraform-aws-retool-ecs-fargate"
+  retool_licence = "12345678-1234-1234-1234-123456789012"
+  vpc_id = "vpc-xxxx"
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
+  retool_alb_subnets = ["subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx"]
+  retool_jobs_runner_ecs_service_subnet = ["subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx"]
+  retool_ecs_service_subnet = ["subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx"]
+  retool_db_subnet_ids = ["subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx", "subnet-xxxx"]
+  retool_custom_host_url = "fancy-url"
+  route_53_zone_id = "Z12345678901234567890"
+}
