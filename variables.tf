fe9a230bf07dbc40a96ebec72aa726383bbdf4e6
Alex Zolin <alex.zolin@actioniq.com>
2024-01-12 13:01:28 -0500

    upgrading RDS engine version

--- variables.tf
+++ variables.tf
@@ -215,7 +215,7 @@ variable "retool_rds_cluster_engine" {
 variable "retool_rds_cluster_engine_version" {
   description = "AWS RDS Cluster engine version - this is despendent on var.retool_rds_cluster_engine"
   type        = number
-  default     = "11.17"
+  default     = "12.16"
 }
 
 variable "retool_rds_cluster_instance_count" {
