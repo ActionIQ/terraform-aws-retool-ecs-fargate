3416afec39d776330ffa9e365edd9ccf7c27e2df
Alex Zolin <55250541+ComradeZo@users.noreply.github.com>
2024-10-02 23:03:42 -0400

    add rds cluster ARN to module outputs

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "retool_url" {
   description = "Valid Retool URL to access on-premise deployment"
   value       = var.retool_custom_host_url != null && var.route_53_zone_id != null ? "${lower(local.retool_alb_listener_protocol)}://${aws_route53_record.retool_custom_host_url[0].fqdn}${local.retool_url_port}" : null
 }
+
+output "retool_rds_instance_arn" {
+  description = "ARN of the Aurora DB instance"
+  value       = aws_rds_cluster.retool_postgresql.arn
+}
